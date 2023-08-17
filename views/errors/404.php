e137b6710e8a3923122a47e420f5b6beb4b5610b
m-rhnm <90450510+m-rhnm@users.noreply.github.com>
2023-08-17 15:28:23 +0330

    Add files via upload

---
+++ views/errors/404.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404: Not Found</title>
+</head>
+<body style="background:#f1f1f1">
+  <h2 style="margin:100px;text-align:center">404<br> Page Not Found... </h2> 
+</body>
+</html>
